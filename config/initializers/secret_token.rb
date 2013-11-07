a0f92f74a06f0c12447b2c69308819ccfda9fe64
Houston Golden <houstongolden@gmail.com>
2013-11-06 22:46:54 -0800

    initialize GAlums new repo - first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Galums::Application.config.secret_key_base = 'a8ea8918cd49d81c4ce0d475571256603ea44f580f0ca66a13f45ff8ec9c14c3d25abba91aba7ba17230dcbf16e587e51b4085e7d58c6526f8c12f38ecda81bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Galums::Application.config.session_store :cookie_store, key: '_Galums_session'
