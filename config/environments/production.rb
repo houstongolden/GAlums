525e9f4ba70f7db48df5e9bef9a9824d47e48545
Houston Golden <houstongolden@gmail.com>
2013-11-21 15:15:39 -0800

    GALUMES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Galums::Application.configure do
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_credentials => {
-        :bucket => ENV['GAlumes'],
+        :bucket => ENV['GAlums'],
         :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 
-Paperclip::Attachment.default_options[:url] = 'galums.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:url] = 'galums.s3.amazonaws.com'
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
