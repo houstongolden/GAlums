c16fb6ee59f66cdf7ee4f76f3aaf39bb7edc089c
Patrick McPike <patrick.mcpike@gmail.com>
2013-11-21 10:52:13 -0800

    Update relationships between User Profile Project models

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,8 @@
 class Profile < ActiveRecord::Base
+	belongs_to :user
+	has_and_belongs_to_many :projects
+	has_many :microposts
+
+	has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
   has_many :authentications
 
+
+  after_create :create_profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def create_profile
+  	user = User.last
+   	profile = Profile.create(contact_email: user.email, user_id: user.id)
+  end 
 
   # Setup accessible (or protected) attributes for your model
   # (Change to Strong Params!) 
